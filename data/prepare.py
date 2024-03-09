b791ad69f0aca4121efb537a7a154ab19c6bbe3c
Sam Foreman <saforem2@gmail.com>
2024-03-08 23:59:03 -0600

    Update `data/prepare.py`

--- data/prepare.py
+++ data/prepare.py
@@ -37,6 +37,79 @@ num_proc_load_dataset = num_proc
 # HF_DATASETS_CACHE="./cache"
 
 
+def load_and_save_dataset(name: str):
+    dataset = load_dataset(name)
+
+    # owt by default only contains the 'train' split, so create a test split
+    # split_dataset = dataset["train"].train_test_split(test_size=0.1,
+    # seed=2357, shuffle=True) split_dataset['val'] = split_dataset.pop('test')
+    # # rename the test split to val
+
+    # this results in:
+    # >>> split_dataset
+    # DatasetDict({
+    #     train: Dataset({
+    #         features: ['text'],
+    #         num_rows: 8009762
+    #     })
+    #     val: Dataset({
+    #         features: ['text'],
+    #         num_rows: 4007
+    #     })
+    # })
+    # we now want to tokenize the dataset. first define the encoding function
+    # (gpt2 bpe)
+    enc = tiktoken.get_encoding("gpt2")
+
+    def process(example):
+        # encode_ordinary ignores any special tokens
+        ids = enc.encode_ordinary(example['text'])
+        # add the end of text token, e.g. 50256 for gpt2 bpe
+        ids.append(enc.eot_token)
+        # note: I think eot should be prepended not appended... hmm.
+        # it's called "eot" though...
+        out = {'ids': ids, 'len': len(ids)}
+        return out
+
+    # tokenize the dataset
+    tokenized = dataset.map(
+        process,
+        remove_columns=['text'],
+        desc="tokenizing the splits",
+        num_proc=num_proc,
+    )
+
+    # concatenate all the ids in each dataset into one large file we can use
+    # for training:
+    # train.bin is ~17GB, val.bin ~8.5MB
+    # train has ~9B tokens (9,035,582,198)
+    # val has ~4M tokens (4,434,897)
+
+    # to read the bin files later, e.g. with numpy:
+    # m = np.memmap('train.bin', dtype=np.uint16, mode='r')
+    outdir = Path(data_dir).joinpath(name)
+    # outdir = Path(os.path.dirname(__file__)).joinpath(name)
+    outdir.mkdir(exist_ok=True, parents=True)
+    print(f'Saving dataset {name} to {outdir}')
+    # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
+
+    for split, dset in tokenized.items():
+        arr_len = np.sum(dset['len'], dtype=np.uint64)
+        filename = outdir.joinpath(f'{split}.bin')
+        dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
+        arr = np.memmap(filename.as_posix(), dtype=dtype, mode='w+', shape=(arr_len,))
+        total_batches = 1024
+        idx = 0
+        for batch_idx in tqdm(range(total_batches), desc=f'writing {filename}'):
+            # Batch together samples for faster write
+            batch = dset.shard(num_shards=total_batches, index=batch_idx, contiguous=True).with_format('numpy')
+            arr_batch = np.concatenate(batch['ids'])
+            # Write into mmap
+            arr[idx : idx + len(arr_batch)] = arr_batch
+            idx += len(arr_batch)
+        arr.flush()
+
+
 if __name__ == '__main__':
     import sys
     args = sys.argv[1:]
